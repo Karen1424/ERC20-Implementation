1964f2b20b7dccfc5bcf3b394849805d536398f7
Karen1424 <vip.karengrigoryan@mail.ru>
2022-05-20 17:55:39 +0400

    "changes"

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -19,23 +19,23 @@ contract ERC20 is IERC20 {
         _symbol = symbols;
     }
 
-    function name() public view virtual returns(string memory) {
+    function name() public view virtual returns (string memory) {
         return _name;
     }
 
-    function symbol() public view virtual returns(string memory) {
+    function symbol() public view virtual returns (string memory) {
         return _symbol;
     }
 
-    function decimals() public view virtual returns(uint8) {
+    function decimals() public view virtual returns (uint8) {
         return 18;
     }
 
-    function totalSupply() public view virtual override returns(uint256) {
+    function totalSupply() public view virtual override returns (uint256) {
         return _totalSupply;
     }
 
-    function balanceOf(address account) public view virtual override returns(uint256) {
+    function balanceOf(address account) public view virtual override returns (uint256) {
         return _balances[account];
     }
 
